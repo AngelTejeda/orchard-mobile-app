a215f6f430becdcc93ed729cb28a1f7fba45d0f4
layefly <64159110+layefly@users.noreply.github.com>
2020-05-26 23:12:56 -0500

    vida kk

--- src/app/acerca-de/acerca-de.page.ts
+++ src/app/acerca-de/acerca-de.page.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import {MenuController} from "@ionic/angular";
 import { Geolocation, Geoposition } from '@ionic-native/geolocation/ngx';
+import { LoadingController } from '@ionic/angular';
+import { analyzeAndValidateNgModules } from '@angular/compiler';
 
 declare var google;
 
@@ -10,8 +12,10 @@ declare var google;
   styleUrls: ['./acerca-de.page.scss'],
 })
 export class AcercaDePage implements OnInit {
+  mapReferencia = null;
 
-  constructor(private geolocation: Geolocation) {
+  constructor(private geolocation: Geolocation,
+    private loadCtrl: LoadingController) {
   }
 
   ngOnInit() {
@@ -19,17 +23,43 @@ export class AcercaDePage implements OnInit {
   }
 
   async loadMap(){
+    const loading = await this.loadCtrl.create();
+    loading.present();
+    const myLatLng = await this.obtenerLocalizacion();
+    console.log(myLatLng);
+    const mapEle: HTMLElement = document.getElementById('map');
+    this.mapReferencia = new google.maps.Map(mapEle, {
+      center: myLatLng,
+      zoom: 12
+    });
+    google.maps.event
+    .addListenerOnce(this.mapReferencia, 'idle', ()=>{
+      //console.log('added');
+      loading.dismiss();
+      this.agregarMarcador(myLatLng.lat, myLatLng.lng);
+    });
+  }
+
+  private agregarMarcador(lat: number, lng: number){
+    const marker = new google.maps.Marker({
+      position:{ 
+        //JS Reconoce que los nombres son iguales, entonces no es necesario asignarlos como valor
+        lat: lat,
+        lng: lng
+      },
+      zoom: 8,
+      map: this.mapReferencia,
+      title: 'Holis'
+    })
+  };
+
+  private async obtenerLocalizacion(){
     const rta = await this.geolocation.getCurrentPosition();
-    const myLatLng = {
+    return {
+      //Es el objeto con la latitud y longitud
       lat: rta.coords.latitude,
       lng: rta.coords.longitude
     };
-    console.log(myLatLng);
-    const mapEle: HTMLElement = document.getElementById('map');
-    const map = new google.maps.Map(mapEle, {
-      center: myLatLng,
-      zoom: 12
-    })
   }
 
 }
