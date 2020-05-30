0a68a25ed2921f07859c25a6fd3267d98722187d
SouAle <smc_gaytan@hotmail.com>
2020-05-30 14:01:38 -0500

    Inicio: Cultivos, Ubicacion y Contacto

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -10,5 +10,26 @@ import {Router} from "@angular/router";
 export class HomePage {
 
     constructor(private menu: MenuController, private router: Router) {}
+    
+    productos = [
+        {
+          nombre: "Producto 1",
+          descripcion: "Este es el producto 1.",
+          imagen: "assets/Productos/Cultivos/papa1.jpg",
+          referencia: "/productos/cultivos"
+        },
+        {
+          nombre: "Producto 2",
+          descripcion: "Este es el producto 2.",
+          imagen: 'assets/Productos/Cultivos/papa1.jpg',
+          referencia: "/productos/frutas"
+        },
+        {
+            nombre: "Producto 3",
+            descripcion: "Este es el producto 3.",
+            imagen: "assets/Productos/Cultivos/papa1.jpg",
+            referencia: "/productos/frutas"
+          }
+      ]
 
 }
